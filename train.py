983b7cb566c96fe2301ec88e777abe7f45470708
Andraž Podpečan <podpecan.andraz28@gmail.com>
2025-05-24 18:57:39 +0200

    Training script improvement and CV configs

--- train.py
+++ train.py
@@ -1,13 +1,20 @@
-import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score, confusion_matrix
+from sklearn import neural_network, model_selection, neighbors, naive_bayes, tree, linear_model, ensemble, metrics, svm
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier, RidgeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.metrics import classification_report, confusion_matrix
 import csv
-from zipfile import ZipFile
 import gzip
 import os
 import argparse
 from pathlib import Path
 import json
+
 
 def load_data(data_file):
     p = Path(data_file)
@@ -17,49 +24,35 @@ def load_data(data_file):
         open_f = open
     else:
         raise ValueError(f"Invalid data file suffix: {p.suffix}")
-    
+
     with open_f(data_file, 'r') as df:
-        reader = csv.reader(df)
-        dim = tuple(map(int, next(reader))) # get width and height from first line
+        dim = tuple(map(int, next(df).split(';')))  # get width and height from first line
+        reader = csv.reader(df, delimiter=';')
         y = []
         x = []
-        
-        for row in reader:
-            y.append(int(row[0]))
-            x.append(list(map(float, row[2:])))
-    
-        return dim, x, y        
-        
-def get_model(m_str: str, m_params: dict | None):
-    if m_params is None:
-        m_params = {}
 
-    # noinspection PyUnreachableCode
-    match m_str:
-        case 'svm':
-            return sklearn.svm.SVC(**m_params)
-        case 'sgd':
-            return sklearn.linear_model.SGDClassifier(**m_params)
-        case 'decision_tree':
-            return sklearn.tree.DecisionTreeClassifier(**m_params)
-        case 'knn':
-            return sklearn.neighbors.KNeighborsClassifier(**m_params)
-        case 'ridge':
-            return sklearn.linear_model.RidgeClassifier(**m_params)
-        case 'gnb':
-            return sklearn.naive_bayes.GaussianNB(**m_params)
-        case 'mlp':
-            return sklearn.neural_network.MLPClassifier(**m_params)
-        case 'ada_boost':
-            return sklearn.ensemble.AdaBoostClassifier(**m_params)
-        case 'grad_boost':
-            return sklearn.ensemble.GradientBoostingClassifier(**m_params)
-        case 'random_forest':
-            return sklearn.ensemble.RandomForestClassifier(**m_params)
-        #     
-        case _:
-            raise ValueError(f"Unknown model string: {m_str}")
-        
+        for row in reader:
+            y.append(int(row[1]))
+            x.append(list(map(float, row[2:])))
+
+        return dim, x, y
+
+
+def get_model(config):
+    model = eval(config['model'])
+    
+    has_hyper = 'hyper' in config
+    has_grid = 'hyper_grid' in config
+    
+    if has_hyper == has_grid:
+        raise ValueError("Config error: 'hyper' and 'hyper_grid' cannot be defined in the same config")
+    
+    if has_hyper:
+        return model(**config['hyper']), False
+    else:
+        return RandomizedSearchCV(model(), config['hyper_grid'], n_iter=10, cv=3, n_jobs=-1), True
+
+
 def main():
     parser = argparse.ArgumentParser("train")
     parser.add_argument('--validate_conf', action='store_true')
@@ -68,9 +61,9 @@ def main():
     parser.add_argument("conf", type=str)
     args = parser.parse_args()
 
-    config = json.load(args.conf)
+    config = json.load(open(args.conf, 'r'))
     try:
-        model = get_model(config['model'], config.get('model_params'))
+        model, hyper_search = get_model(config)
     except Exception as ex:
         print(ex)
         exit(1)
@@ -79,32 +72,36 @@ def main():
         exit(0)
 
     dim, x, y = load_data(args.data)
-    
+
     metaparams = {
-        "test_size": 0.4
+        "test_size": 0.2
     }
-    
+
     metaparams.update(config.get("metaparams", {}))
-    
-    x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=metaparams['test_size'])
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=metaparams['test_size'])
     model.fit(x_train, y_train)
     
+    if hyper_search:
+        clf = model.best_estimator_
+    else:
+        clf = model
+
     # results
-    y_pred = model.predict(y_test)
-    
+    y_pred = clf.predict(x_test)
+
     results = {
-        'precision': precision_score(y_test, y_pred),
-        'recall': recall_score(y_test, y_pred),
-        'accuracy': recall_score(y_test, y_pred),
-        'f1': f1_score(y_test, y_pred),
-        'confusion_matrix': confusion_matrix(y_test, y_pred)
+        "hyperparams": clf.get_params(),
+        "classification_report": classification_report(y_test, y_pred, output_dict=True),
+        "confusion_matrix": confusion_matrix(y_test, y_pred).tolist()
     }
-    
+
     try:
-        json.dump(results, args.results, ensure_ascii=False)
+        with open(args.results, 'w', encoding='utf-8') as f:
+            json.dump(results, f, ensure_ascii=False)
     except ValueError:
         print(results)
-    
-    
+
+
 if __name__ == "__main__":
     main()
